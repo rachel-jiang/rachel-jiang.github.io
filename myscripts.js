6bfb33c4be269431e49f78e895c75019b5e4703d
rachel-jiang <71416122+rachel-jiang@users.noreply.github.com>
2021-04-01 11:54:31 -0300

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,660 @@
+
+$(document).ready(function(){ //always start with this, it gets the document when ready
+  
+  //li=["#domestic-cats","#wildcats","#felidae","#bengal-cats"]
+  
+  //hide pages
+  //function hideRest(){
+    //$("#domestic-cats","#wildcats","#felidae","#bengal-cats").hide()
+  //}
+  
+  //hide homepage
+  function hideHomepage(){
+    $("#homepage").hide()
+  }
+
+  //show pages
+  function showHomepage(){ 
+    $("#homepage").show()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#ragdoll").hide()
+    $("#american-shorthair").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+}
+
+  function showDoCats(){
+    hideHomepage()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#domestic-cats").show()
+  }
+
+  function showWiCats(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#wildcats").show()
+  }
+
+  function showBiCats(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#big-cats").show()
+  }
+
+  function showBenCat(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#bengal-cats").show()
+  }
+
+  function showAmerShor(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#american-shorthair").show()
+  }
+
+  function showRagdoll(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#ragdoll").show()
+  }
+
+  function showBriShor(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#british-shorthair").show()
+  }
+
+  function showScoFol(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#scottish-fold").show()
+  }
+
+  function showChet(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#cheetoh").show()
+  }
+
+  function showEgyM(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#egyptian-mau").show()
+  }
+
+  function showSava(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#savannah-cat").show()
+  }
+
+  function showAfriWi(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#serval").hide()
+    $("#lion").hide()
+    $("#lynx").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#african-wildcat").show()
+  }
+
+  function showServal(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#lion").hide()
+    $("#lynx").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#serval").show()
+  }
+
+  function showLion(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lynx").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#lion").show()
+  }
+
+  function showLynx(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lion").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#lynx").show()
+  }
+
+  function showLeopard(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lion").hide()
+    $("#lynx").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#leopard").show()
+  }
+
+  function showTiger(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lion").hide()
+    $("#lynx").hide()
+    $("#leopard").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#tiger").show()
+  }
+
+  function showCheetah(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lion").hide()
+    $("#lynx").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#cheetah").show()
+  }
+
+  function showJaguar(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lion").hide()
+    $("#lynx").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#black-panther").hide()
+    $("#url-page").hide()
+    $("#jaguar").show()
+  }
+
+  function showBPan(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lion").hide()
+    $("#lynx").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#url-page").hide()
+    $("#black-panther").show()
+  }
+
+  function showURLPage(){
+    hideHomepage()
+    $("#domestic-cats").hide()
+    $("#wildcats").hide()
+    $("#big-cats").hide()
+    $("#bengal-cats").hide()
+    $("#american-shorthair").hide()
+    $("#ragdoll").hide()
+    $("#british-shorthair").hide()
+    $("#scottish-fold").hide()
+    $("#cheetoh").hide()
+    $("#egyptian-mau").hide()
+    $("#savannah-cat").hide()
+    $("#african-wildcat").hide()
+    $("#serval").hide()
+    $("#lion").hide()
+    $("#lynx").hide()
+    $("#leopard").hide()
+    $("#tiger").hide()
+    $("#cheetah").hide()
+    $("#jaguar").hide()
+    $("#black-panther").hide()
+    $("#url-page").show()
+  }
+
+
+  //call function
+  showHomepage() //this hides every other pages at the beginning, only shows the homepage
+
+  //go to category pages
+  $("#do-but").on("click",showDoCats) //domestic cats page
+  $("#wi-but").on("click",showWiCats) //wildcats page
+  $("#big-but").on("click",showBiCats) //big cats page
+
+  //go to information pages
+  $("#ben-cat-but").on("click",showBenCat) //bengal cats page
+  $("#american-shorthair-but").on("click",showAmerShor) //american shorthair page
+  $("#ragdoll-but").on("click",showRagdoll) //ragdoll page
+  $("#british-shorthair-but").on("click",showBriShor) //british shorthair page
+  $("#scottish-fold-but").on("click",showScoFol) //scottish fold page
+  $("#cheetoh-but").on("click",showChet) //cheetoh page
+  $("#egypt-mau-but").on("click",showEgyM) //egyptian mau page
+  $("#sava-cat-but").on("click",showSava) //savannah cat page
+  $("#afri-wcat-but").on("click",showAfriWi) //african wildcat page
+  $("#serval-but").on("click",showServal) //serval page
+  $("#lion-but").on("click",showLion) //lion page
+  $("#lynx-but").on("click",showLynx) //lynx page
+  $("#leopard-but").on("click",showLeopard)//leopard page
+  $("#tiger-but").on("click",showTiger)//tiger page
+  $("#cheetah-but").on("click",showCheetah) //cheetah page
+  $("#jaguar-but").on("click",showJaguar) //jaguar page
+  $("#b-pan-but").on("click",showBPan) //black panther page
+
+  //go to url page
+  $("#search-web").on("click",showURLPage) //url page
+
+  //back to homepage buttons
+  $("#back").on("click",showHomepage)
+  $("#back1").on("click",showHomepage)
+  $("#back2").on("click",showHomepage)
+  $("#back3").on("click",showHomepage)
+  $("#back4").on("click",showHomepage)
+  $("#back5").on("click",showHomepage)
+  $("#back6").on("click",showHomepage)
+  $("#back7").on("click",showHomepage)
+  $("#back8").on("click",showHomepage)
+  $("#back9").on("click",showHomepage)
+  $("#back10").on("click",showHomepage)
+  $("#back11").on("click",showHomepage)
+  $("#back12").on("click",showHomepage)
+  $("#back13").on("click",showHomepage)
+  $("#back14").on("click",showHomepage)
+  $("#back15").on("click",showHomepage)
+  $("#back16").on("click",showHomepage)
+  $("#back17").on("click",showHomepage)
+  $("#back18").on("click",showHomepage)
+  $("#back19").on("click",showHomepage)
+  $("#back20").on("click",showHomepage)
+  $("#back21").on("click",showHomepage)
+  $("#back22").on("click",showHomepage)
+  $("#back23").on("click",showHomepage)
+  $("#back24").on("click",showHomepage)
+  $("#back25").on("click",showHomepage)
+  $("#back26").on("click",showHomepage)
+  $("#back27").on("click",showHomepage)
+  $("#back28").on("click",showHomepage)
+  $("#back29").on("click",showHomepage)
+  $("#back30").on("click",showHomepage)
+  $("#back31").on("click",showHomepage)
+
+
+
+
+//search to google web 
+  function SerWeb(){
+    let fir='https://www.google.com/search?q=';
+    let sec='&rlz=1C1GCEA_enCA862CA862&oq=';
+    let thir='&aqs=chrome..69i57j0i10i433j0i10l4j0i390l2.3786j0j7&sourceid=chrome&ie=UTF-8';
+    var adInp= document.getElementById("userInput");
+    let finalURL= fir + strint(adInp) + sec + string(adInp) + thir;
+    alert(finalURL);
+  }
+  $("#search-web").on("click",SerWeb)
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
